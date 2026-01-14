b19a39074f9bd5378fed7ce71bade4376b5896f7
4drea4 <bakerand@outlook.com>
2026-01-14 14:02:02 -0600

    did somethings out of order, cleaning up now

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ const app = express();
 
 //Create a single GET route at / that, upon a successful database connection, sends back a JSON response: { message: "Successfully connected to the database!" }. If the connection fails, it should send a 500 status code with a message: { message: "Failed to connect to the database." }.
 app.get('/',  async (req,res)=>{
-    
-        const client.connect();
+    const client = new MongoClient(process.env.MONGO_URI);
+
+    try{
+        await client.connect();
         return res.json({message:  "Successfully connected to the database"});
+    }
+        
+        
 } catch (error)
     return res.status(500).json({message:"Failed to connect to the database. "})
